3580fc166df6a0fe0fdd4eb8f6a789431d7adc1e
CheranMahalingam <65926174+CheranMahalingam@users.noreply.github.com>
2021-02-20 13:48:21 -0500

    Setup firebase authentication

--- client/src/components/LoginPage.js
+++ client/src/components/LoginPage.js
@@ -1,9 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import firebase from "../config.js";
 
 export default function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  let history = useHistory();
+
+  const handleEmail = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePassword = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleLogin = () => {
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .then(() => {
+        history.push("/map");
+      })
+      .catch((error) => {
+        alert(error);
+      });
+  };
+
   return (
-    <div>
+    <div className="login-page">
       <p>Login Page</p>
+      <br />
+      <input placeholder="Email" type="text" onChange={handleEmail} />
+      <br />
+      <input placeholder="Password" type="password" onChange={handlePassword} />
+      <br />
+      <button className="button-login" onClick={handleLogin}>
+        Login
+      </button>
     </div>
   );
 }
--- client/src/components/SignUpPage.js
+++ client/src/components/SignUpPage.js
@@ -1,9 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import firebase from "../config.js";
 
 export default function SignUpPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  let history = useHistory();
+
+  const handleEmail = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePassword = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleRegistration = () => {
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then(() => {
+        history.push("/login");
+      })
+      .catch((error) => {
+        alert(error);
+      });
+  };
+
   return (
-    <div>
+    <div className="signup-page">
       <p>Sign Up Page</p>
+      <br />
+      <input placeholder="First Name" type="text" />
+      <br />
+      <input placeholder="Last Name" type="text" />
+      <br />
+      <input placeholder="Email" type="text" onChange={handleEmail} />
+      <br />
+      <input placeholder="Password" type="password" onChange={handlePassword} />
+      <br />
+      <button className="button-submit" onClick={handleRegistration}>
+        Submit
+      </button>
     </div>
   );
 }
